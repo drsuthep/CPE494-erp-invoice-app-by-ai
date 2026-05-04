193efdef5559bc5fc23c5680ec07b308323f0abf
Suthep Madarasmi <suthepmail@gmail.com>
2026-05-04 18:29:25 +0700

    Apply Sprint 1 foundation shell and Zen Green styles

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,17 +1,9 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace CPE494_erp_invoice_app_by_ai.Pages;
 
 public class IndexModel : PageModel
 {
-    private readonly ILogger<IndexModel> _logger;
-
-    public IndexModel(ILogger<IndexModel> logger)
-    {
-        _logger = logger;
-    }
-
     public void OnGet()
     {
 
